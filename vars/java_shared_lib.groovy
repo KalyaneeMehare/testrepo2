66e888692d13b238c9a0dde8b521fdbc3045772b
KalyaneeMehare <108780271+KalyaneeMehare@users.noreply.github.com>
2022-07-17 20:14:34 +0530

    Update java_shared_lib.groovy

--- vars/java_shared_lib.groovy
+++ vars/java_shared_lib.groovy
@@ -1,121 +1,61 @@
 def call(String repoUrl,String gitu,String sonarname,String sonarid,String branch) {
 def choice=[]
-node {
-    choice = params["build"].split(",")
-}
 pipeline {
-		agent {
-		docker {
-			image 'maven:3.8.1-jdk-11'
-			args '-v /root/.m2:/root/.m2'
-			args '-v /opt/:/opt/'
-			}
-			}
-
-	
-//	parameters {
-//        choice(
-//            choices: ['true' , 'false'],
-//            description: '',
-//            name: 'REQUESTED_ACTION')
-//    }
-// 	environment {          
-//         def p = 'params.build'
-// 		xyz = p.split(',,');
-// 	}	
-// 	
-// 		
-	stages {
-			stage("Checkout Code") {
-               steps {
-		       git branch: "${branch}",
-	           credentialsId: "${gitu}",
-                    url: "${repoUrl}"
-               }
-           }
-			stage('compile') {
-// 				String[] str ;
-// 				str = env.{params.build}.split(',');
-// 				for ( String str in [ 'compile,build,junit-test,integration-test,sonarqube-integration,packing,renaming-the-jar,backup' ]) {
-				//for (compile in ['params.build']) {
-				//for ( compile in [xyz] ) {
-				when {
-					expression {'compile' in choice }
-				}
-				steps {
-				       sh 'mvn compile'
-				}
-		}
-			stage('Build') {
-				//for (build in [xyz]) {
-				//for ( "compile" in ${str}) {
-				when  {
-					expression { 'build' in choice }
-				}
-				steps {
-				      sh 'mvn clean install'
-			}
-		}
-                        stage('junit-test') {
-				when {
-					expression { 'junit-test' in choice }
-				}
-                                steps {
-                                       sh 'mvn test'
-                        }
-        	        post {
- 	               always {
-                	    junit 'target/*.xml'
-         	      	 }
-           	 }
- 	    }
-			stage('integration-testing') {
-				when {
-					expression { 'integration-test' in choice }
-            }
-				steps {
-				      sh 'mvn test -Dtest=**/*IT.java'
-			}
-		}
-		stage('sonarqube-integration') {
-			when {
-                expression { 'sonarqube-integration' in choice }
-            }
-				steps {
-					withSonarQubeEnv(credentialsId: 'sonar-pro', installationName: 'MySonar'){
-						sh 'java -jar jacococli.jar report target/jacoco.exec --classfiles target/  --xml target/report.xml'
-				       sh 'mvn verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar -Dsonar.projectKey=sonar-pro1 -Dsonar.login=ee1b7f8c900e2ca73ceb9fe326e396fa07b6774c -Dsonar.host.url=https://sonarcloud.io/'
-				       sh 'sleep 60'
-			}
-		}
-			}
-			stage('package')
-                             {
-				     when {
-                expression { 'package' in choice }
-            }
-                                steps
-                                     {
-                                       sh 'mvn package'
-                                     }
-	                     }
-			stage('renaming-the-jar') {
-				when {
-                expression { 'renaming-the-jar' in choice }
-            }
-				steps {
-				      sh 'mkdir -p /opt/backup'
-				      sh 'mv target/sampleapp-1.0.0-SNAPSHOT-LOCAL.jar target/java-jar.${BUILD_NUMBER}.jar'
-			}
-		}
-			stage('backing-up-the-jar') {
-				when {
-                expression { 'backing-up-the-jar' in choice }
-            }
-				steps {
-				      sh 'cp target/java-jar.${BUILD_NUMBER}.jar /opt/backup/'
-			}
-		}	
+    agent any
+    tools {
+    	maven 'My_Maven'
 	}
-}
+    stages {
+        stage('Git Checkout') {
+            steps {
+                git branch: "${branch}",
+		credentialsId: "${gitu}",
+                url: "${repoUrl}"
+            }
+        }    
+        stage('Compile') {
+            steps {
+                sh 'mvn clean install'
+            }
+        }
+        stage('Package') {
+            steps {
+                sh 'mvn package'
+            }
+        }
+        stage('Unit Test') {
+            steps {
+                sh 'mvn test'
+            }
+        }
+	stage("build & SonarQube analysis") {
+            steps {
+             withSonarQubeEnv(credentialsId: 'sonar-pro', installationName: 'MySonar'){
+		sh 'mvn verify sonar:sonar -Dsonar.projectKey=sonar-pro1 -Dsonar.login=ee1b7f8c900e2ca73ceb9fe326e396fa07b6774c -Dsonar.host.url=https://sonarcloud.io/'
+		sh 'sleep 60'
+		}
+            }
+          }
+	  stage("Quality Gate") {
+             steps {
+               timeout(time: 1, unit: 'HOURS') {
+                 waitForQualityGate abortPipeline: true
+              }
+            }
+          }
+	 
+	  stage("renaming-the-jar-and-backing-up-the-jar") {
+            steps {
+        	sh 'sudo mkdir -p /opt/backup'
+       	        sh 'sudo mv target/my-app-1.0-SNAPSHOT.jar target/java-jar.${BUILD_NUMBER}.jar'
+                sh 'sudo cp target/java-jar.${BUILD_NUMBER}.jar /opt/backup/'
+           
+		}
+	     }
+	     stage('Deploy To Environment') {
+                steps {
+                 sh 'echo Deployment Done'
+            }
+        }
+    }
 }
